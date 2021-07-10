f3eefc98cc81745c0a4d2b593c7f132aadae2470
Tyler Pena <tylerpena@me.com>
2021-07-09 22:00:09 -0700

    try catch process latest blocks

--- src/Opdex.Platform.Application/EntryHandlers/Blocks/ProcessLatestBlocksCommandHandler.cs
+++ src/Opdex.Platform.Application/EntryHandlers/Blocks/ProcessLatestBlocksCommandHandler.cs
@@ -36,54 +36,61 @@ namespace Opdex.Platform.Application.EntryHandlers.Blocks
 
         public async Task<Unit> Handle(ProcessLatestBlocksCommand request, CancellationToken cancellationToken)
         {
-            // The latest synced block we have, if none, the tip of cirrus chain
-            var previousBlock = await _mediator.Send(new GetBestBlockQuery(), cancellationToken);
-
-            // Process each block until we reach the chain tip
-            while (previousBlock?.NextBlockHash != null && !cancellationToken.IsCancellationRequested)
+            try
             {
-                // Retrieve and create the block
-                var currentBlock = await _mediator.Send(new RetrieveCirrusBlockByHashQuery(previousBlock.NextBlockHash));
-                var blockCreated = await _mediator.Send(new CreateBlockCommand(currentBlock));
+                // The latest synced block we have, if none, the tip of cirrus chain
+                var previousBlock = await _mediator.Send(new GetBestBlockQuery(), cancellationToken);
 
-                if (!blockCreated) break;
-
-                if (currentBlock.IsNewMinuteFromPrevious(previousBlock.MedianTime))
+                // Process each block until we reach the chain tip
+                while (previousBlock?.NextBlockHash != null && !cancellationToken.IsCancellationRequested)
                 {
-                    // Dev Environment = 15 minutes, otherwise 1 minute
-                    if (request.NetworkType != NetworkType.DEVNET || currentBlock.MedianTime.Minute % 15 == 0)
+                    // Retrieve and create the block
+                    var currentBlock = await _mediator.Send(new RetrieveCirrusBlockByHashQuery(previousBlock.NextBlockHash));
+                    var blockCreated = await _mediator.Send(new CreateBlockCommand(currentBlock));
+
+                    if (!blockCreated) break;
+
+                    if (currentBlock.IsNewMinuteFromPrevious(previousBlock.MedianTime))
                     {
-                        await _mediator.Send(new CreateCrsTokenSnapshotsCommand(currentBlock.MedianTime));
+                        // Dev Environment = 15 minutes, otherwise 1 minute
+                        if (request.NetworkType != NetworkType.DEVNET || currentBlock.MedianTime.Minute % 15 == 0)
+                        {
+                            await _mediator.Send(new CreateCrsTokenSnapshotsCommand(currentBlock.MedianTime));
+                        }
                     }
+
+                    var crs = await _mediator.Send(new RetrieveTokenByAddressQuery(TokenConstants.Cirrus.Address));
+
+                    var crsSnapshot = await _mediator.Send(new RetrieveTokenSnapshotWithFilterQuery(crs.Id, 0, currentBlock.MedianTime, SnapshotType.Minute));
+
+                    // If it's a new day from the previous block, refresh all daily snapshots. (Tokens, Liquidity Pools, Markets)
+                    if (currentBlock.IsNewDayFromPrevious(previousBlock.MedianTime))
+                    {
+                        await _mediator.Send(new ProcessDailySnapshotRefreshCommand(currentBlock.MedianTime, crsSnapshot.Price.Close));
+                    }
+
+                    // Process all transactions in the block
+                    foreach (var tx in currentBlock.TxHashes.Where(tx => tx != currentBlock.MerkleRoot))
+                    {
+                        // Todo: Consider processing liquidity pool snapshots after each block rather than during each transaction.
+                        await _mediator.Send(new CreateTransactionCommand(tx));
+                    }
+
+                    // Get and process all available Opdex markets
+                    // Todo: Consider only updating those that had transactions in the block being processed.
+                    var markets = await _mediator.Send(new RetrieveAllMarketsQuery());
+
+                    foreach (var market in markets)
+                    {
+                        await _mediator.Send(new ProcessMarketSnapshotsCommand(market.Id, currentBlock.MedianTime));
+                    }
+
+                    previousBlock = currentBlock;
                 }
-
-                var crs = await _mediator.Send(new RetrieveTokenByAddressQuery(TokenConstants.Cirrus.Address));
-
-                var crsSnapshot = await _mediator.Send(new RetrieveTokenSnapshotWithFilterQuery(crs.Id, 0, currentBlock.MedianTime, SnapshotType.Minute));
-
-                // If it's a new day from the previous block, refresh all daily snapshots. (Tokens, Liquidity Pools, Markets)
-                if (currentBlock.IsNewDayFromPrevious(previousBlock.MedianTime))
-                {
-                    await _mediator.Send(new ProcessDailySnapshotRefreshCommand(currentBlock.MedianTime, crsSnapshot.Price.Close));
-                }
-
-                // Process all transactions in the block
-                foreach (var tx in currentBlock.TxHashes.Where(tx => tx != currentBlock.MerkleRoot))
-                {
-                    // Todo: Consider processing liquidity pool snapshots after each block rather than during each transaction.
-                    await _mediator.Send(new CreateTransactionCommand(tx));
-                }
-
-                // Get and process all available Opdex markets
-                // Todo: Consider only updating those that had transactions in the block being processed.
-                var markets = await _mediator.Send(new RetrieveAllMarketsQuery());
-
-                foreach (var market in markets)
-                {
-                    await _mediator.Send(new ProcessMarketSnapshotsCommand(market.Id, currentBlock.MedianTime));
-                }
-
-                previousBlock = currentBlock;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failure processing blocks");
             }
 
             return Unit.Value;
