d2d0b9c215906623723738a9155b256dc8c8866c
Tyler Pena <tylerpena@me.com>
2021-07-25 14:39:23 -0700

    comments

--- src/Opdex.Platform.Application.Abstractions/EntryQueries/Addresses/GetAddressBalanceByTokenQuery.cs
+++ src/Opdex.Platform.Application.Abstractions/EntryQueries/Addresses/GetAddressBalanceByTokenQuery.cs
@@ -5,8 +5,17 @@ using System;
 
 namespace Opdex.Platform.Application.Abstractions.EntryQueries.Addresses
 {
+    /// <summary>
+    /// Get the balance of a wallet address and specific token.
+    /// </summary>
     public class GetAddressBalanceByTokenQuery : IRequest<AddressBalanceDto>
     {
+        /// <summary>
+        /// Constructor for get balance query.
+        /// </summary>
+        /// <param name="walletAddress">The wallet address to check the balance of.</param>
+        /// <param name="tokenAddress">The token to check the wallet balance of.</param>
+        /// <exception cref="ArgumentNullException">Argument null exception for invalid request parameters.</exception>
         public GetAddressBalanceByTokenQuery(string walletAddress, string tokenAddress)
         {
             WalletAddress = walletAddress.HasValue() ? walletAddress : throw new ArgumentNullException(nameof(walletAddress), "Wallet address must be set.");
--- src/Opdex.Platform.Infrastructure.Abstractions/Data/Queries/Addresses/SelectAddressBalanceByOwnerAndTokenIdQuery.cs
+++ src/Opdex.Platform.Infrastructure.Abstractions/Data/Queries/Addresses/SelectAddressBalanceByOwnerAndTokenIdQuery.cs
@@ -5,8 +5,19 @@ using Opdex.Platform.Domain.Models.Addresses;
 
 namespace Opdex.Platform.Infrastructure.Abstractions.Data.Queries.Addresses
 {
+    /// <summary>
+    /// Selects an address' balance of an SRC token from the database.
+    /// </summary>
     public class SelectAddressBalanceByOwnerAndTokenIdQuery : FindQuery<AddressBalance>
     {
+        /// <summary>
+        /// Constructor for the select address balance request.
+        /// </summary>
+        /// <param name="owner">The owner of the tokens wallet address</param>
+        /// <param name="tokenId">The Id of the token</param>
+        /// <param name="findOrThrow">Defaulted to true, optionally throw if a record is not found.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the owner parameter is invalid.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the tokenId provided is invalid.</exception>
         public SelectAddressBalanceByOwnerAndTokenIdQuery(string owner, long tokenId, bool findOrThrow = true) : base(findOrThrow)
         {
             if (!owner.HasValue())
