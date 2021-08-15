54086c8107d1520ff7dda629c79567d121b01873
drmathias <adam.shirt@developmomentum.com>
2021-08-15 13:59:51 +0100

    MinedToken address in response model

--- src/Opdex.Platform.Application/Assemblers/MiningGovernanceDtoAssembler.cs
+++ src/Opdex.Platform.Application/Assemblers/MiningGovernanceDtoAssembler.cs
@@ -1,13 +1,10 @@
 using MediatR;
 using Opdex.Platform.Application.Abstractions.Models.Governances;
-using Opdex.Platform.Application.Abstractions.Models.TokenDtos;
 using Opdex.Platform.Application.Abstractions.Queries.Blocks;
 using Opdex.Platform.Application.Abstractions.Queries.Tokens;
-using Opdex.Platform.Common.Constants;
 using Opdex.Platform.Common.Constants.SmartContracts;
 using Opdex.Platform.Common.Extensions;
 using Opdex.Platform.Domain.Models.Governances;
-using Opdex.Platform.Domain.Models.Tokens;
 using System;
 using System.Threading.Tasks;
 
@@ -16,18 +13,15 @@ namespace Opdex.Platform.Application.Assemblers
     public class MiningGovernanceDtoAssembler : IModelAssembler<MiningGovernance, MiningGovernanceDto>
     {
         private readonly IMediator _mediator;
-        private readonly IModelAssembler<Token, TokenDto> _tokenAssembler;
 
-        public MiningGovernanceDtoAssembler(IMediator mediator, IModelAssembler<Token, TokenDto> tokenAssembler)
+        public MiningGovernanceDtoAssembler(IMediator mediator)
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-            _tokenAssembler = tokenAssembler ?? throw new ArgumentNullException(nameof(tokenAssembler));
         }
 
         public async Task<MiningGovernanceDto> Assemble(MiningGovernance governance)
         {
             var token = await _mediator.Send(new RetrieveTokenByIdQuery(governance.TokenId));
-            var tokenDto = await _tokenAssembler.Assemble(token);
 
             const uint miningPoolsPerYear = GovernanceConstants.MiningPoolsFundedPerYear;
             const uint maxNominations = GovernanceConstants.MaxNominations;
@@ -37,16 +31,18 @@ namespace Opdex.Platform.Application.Assemblers
                 ? governance.NominationPeriodEnd - currentBlock.Height
                 : 0;
 
+            var totalRewardsPerPeriod = governance.MiningPoolReward.ToBigInteger() * maxNominations;
+
             return new MiningGovernanceDto
             {
                 Address = governance.Address,
-                MinedToken = tokenDto,
+                MinedToken = token.Address,
                 PeriodEndBlock = governance.NominationPeriodEnd,
                 PeriodRemainingBlocks = remainingNominationPeriodBlocks,
                 PeriodBlockDuration = governance.MiningDuration,
-                MiningPoolRewardPerPeriod = governance.MiningPoolReward,
+                MiningPoolRewardPerPeriod = governance.MiningPoolReward.InsertDecimal(token.Decimals),
                 PeriodsUntilRewardReset = (miningPoolsPerYear - governance.MiningPoolsFunded) / maxNominations,
-                TotalRewardsPerPeriod = (governance.MiningPoolReward.ToBigInteger() * maxNominations).ToString()
+                TotalRewardsPerPeriod = totalRewardsPerPeriod.ToString().InsertDecimal(token.Decimals)
             };
         }
     }
