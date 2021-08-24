cf813e244020e459b2caa98552d6e5fafa2e519b
drmathias <adam.shirt@developmomentum.com>
2021-08-24 20:08:09 +0100

    [PAPI-146] Remove converter AllowNullAttribute

--- src/Opdex.Platform.Common/Converters/FixedDecimalConverter.cs
+++ src/Opdex.Platform.Common/Converters/FixedDecimalConverter.cs
@@ -1,18 +1,17 @@
 using Newtonsoft.Json;
 using Opdex.Platform.Common.Models;
 using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Opdex.Platform.Common.Converters
 {
     public class FixedDecimalConverter : JsonConverter<FixedDecimal>
     {
-        public override FixedDecimal ReadJson(JsonReader reader, Type objectType, [AllowNull] FixedDecimal existingValue, bool hasExistingValue, JsonSerializer serializer)
+        public override FixedDecimal ReadJson(JsonReader reader, Type objectType, FixedDecimal existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
             return FixedDecimal.Parse((string)reader.Value);
         }
 
-        public override void WriteJson(JsonWriter writer, [AllowNull] FixedDecimal value, JsonSerializer serializer)
+        public override void WriteJson(JsonWriter writer, FixedDecimal value, JsonSerializer serializer)
         {
             writer.WriteValue(value.ToString());
         }
