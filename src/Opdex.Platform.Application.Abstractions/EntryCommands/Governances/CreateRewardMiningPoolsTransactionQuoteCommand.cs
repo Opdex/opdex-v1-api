26c61bfe5149f55323f8ec3360196ccc719ea8dc
Tyler Pena <tylerpena@me.com>
2021-09-12 12:15:48 -0700

    fix comment

--- src/Opdex.Platform.Application.Abstractions/EntryCommands/Governances/CreateRewardMiningPoolsTransactionQuoteCommand.cs
+++ src/Opdex.Platform.Application.Abstractions/EntryCommands/Governances/CreateRewardMiningPoolsTransactionQuoteCommand.cs
@@ -5,17 +5,16 @@ using System;
 namespace Opdex.Platform.Application.Abstractions.EntryCommands.Governances
 {
     /// <summary>
-    ///
+    /// Create a quote for rewarding mining pools by distributing governance tokens to mine.
     /// </summary>
     public class CreateRewardMiningPoolsTransactionQuoteCommand : BaseTransactionQuoteCommand
     {
         /// <summary>
-        ///
+        /// Creates a reward mining pools transaction quote.
         /// </summary>
-        /// <param name="governance"></param>
-        /// <param name="wallet"></param>
-        /// <param name="fullDistribution"></param>
-        /// <exception cref="ArgumentNullException"></exception>
+        /// <param name="governance">The governance contract address to call.</param>
+        /// <param name="wallet">The wallet address public key sending the transaction.</param>
+        /// <param name="fullDistribution">Flag determining if one or all nominated mining pools should be rewarded.</param>
         public CreateRewardMiningPoolsTransactionQuoteCommand(Address governance, Address wallet, bool fullDistribution) : base(wallet)
         {
             if (governance == Address.Empty)
