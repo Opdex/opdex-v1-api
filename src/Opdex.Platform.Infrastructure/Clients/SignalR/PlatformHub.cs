6b2048ee475017236254b5722b4789ccf7b19d03
drmathias <adam.shirt@developmomentum.com>
2021-09-12 14:40:32 +0100

    Hub authentication

--- src/Opdex.Platform.Infrastructure/Clients/SignalR/PlatformHub.cs
+++ src/Opdex.Platform.Infrastructure/Clients/SignalR/PlatformHub.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Opdex.Platform.Infrastructure.Clients.SignalR
 {
+    [Authorize]
     public class PlatformHub : Hub<IPlatformClient>
     {
     }
