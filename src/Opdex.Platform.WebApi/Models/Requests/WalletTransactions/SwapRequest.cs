c5a4520cd0f242f7441a40f24da04ccb1a6f24ec
Tyler Pena <tylerpena@me.com>
2021-10-02 19:42:47 -0700

    swap transaction quote tests

--- src/Opdex.Platform.WebApi/Models/Requests/WalletTransactions/SwapRequest.cs
+++ src/Opdex.Platform.WebApi/Models/Requests/WalletTransactions/SwapRequest.cs
@@ -1,4 +1,5 @@
 using Opdex.Platform.Common.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Opdex.Platform.WebApi.Models.Requests.WalletTransactions
 {
@@ -7,36 +8,43 @@ namespace Opdex.Platform.WebApi.Models.Requests.WalletTransactions
         /// <summary>
         /// The address of the token being retrieved. Null if CRS
         /// </summary>
+        [Required]
         public Address TokenOut { get; set; }
 
         /// <summary>
         /// The amount of tokens to swap
         /// </summary>
+        [Required]
         public FixedDecimal TokenInAmount { get; set; }
 
         /// <summary>
         /// The amount of tokens to receive
         /// </summary>
+        [Required]
         public FixedDecimal TokenOutAmount { get; set; }
 
         /// <summary>
         /// Flag either token in amount is exact or token out amount
         /// </summary>
+        [Required]
         public bool TokenInExactAmount { get; set; }
 
         /// <summary>
         /// The maximum amount of tokens willing to swap
         /// </summary>
+        [Required]
         public FixedDecimal TokenInMaximumAmount { get; set; }
 
         /// <summary>
         /// The minimum amount of tokens acceptable to receive
         /// </summary>
+        [Required]
         public FixedDecimal TokenOutMinimumAmount { get; set; }
 
         /// <summary>
         /// The recipient of the swapped tokens.
         /// </summary>
+        [Required]
         public Address Recipient { get; set; }
 
         /// <summary>
