0f6d492597d0228bd26fc8b3dffee16d1bc507d9
Tyler Pena <tylerpena@me.com>
2021-10-21 11:45:59 -0700

    transaction receipt fixes related to stratis FN updates

--- src/Opdex.Platform.Infrastructure.Abstractions/Clients/CirrusFullNodeApi/Models/Transactions/TransactionLogDetailsDto.cs
+++ src/Opdex.Platform.Infrastructure.Abstractions/Clients/CirrusFullNodeApi/Models/Transactions/TransactionLogDetailsDto.cs
@@ -1,13 +1,13 @@
 using Opdex.Platform.Common.Models;
 
-namespace Opdex.Platform.Infrastructure.Abstractions.Clients.CirrusFullNodeApi.Models
+namespace Opdex.Platform.Infrastructure.Abstractions.Clients.CirrusFullNodeApi.Models.Transactions
 {
-    public class TransactionLogDto
+    public class TransactionLogSummaryDto
     {
         public Address Address { get; set; }
         public string[] Topics { get; set; }
         public string Data { get; set; }
-        public object Log { get; set; }
+        public TransactionLogEventDto Log { get; set; }
         public int SortOrder { get; set; }
     }
 }
