2967902fe4ea25d908e7de2e3105f02c94b01b46
Tyler Pena <tylerpena@me.com>
2021-10-26 11:36:54 -0700

    typo fix

--- src/Opdex.Platform.Common/Enums/TokenAttributeType.cs
+++ src/Opdex.Platform.Common/Enums/TokenAttributeType.cs
@@ -3,7 +3,7 @@ namespace Opdex.Platform.Common.Enums
     public enum TokenAttributeType
     {
         /// <summary>
-        /// Considered s security
+        /// Considered a security
         /// </summary>
         Security = 1
     }
