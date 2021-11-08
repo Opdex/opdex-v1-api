d8b4c95d1c4a4eaa9a588b7ad1c18c6f8d3a6b6f
drmathias <adam.shirt@developmomentum.com>
2021-11-08 20:38:50 +0000

    [PAPI-170] IP rate limiting

---
+++ src/Opdex.Platform.WebApi/Exceptions/TooManyRequestsException.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Opdex.Platform.WebApi.Exceptions
+{
+    public class TooManyRequestsException : Exception
+    {
+        public TooManyRequestsException(double limit, string period, string retryAfter)
+        {
+            Limit = limit;
+            Period = period;
+            RetryAfter = retryAfter;
+        }
+
+        public double Limit { get; }
+        public string Period { get; }
+        public string RetryAfter { get; }
+    }
+}
