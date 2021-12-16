c3d29e7fe87228115b98228cda5b36efeb90cebf
drmathias <adam@opdex.com>
2021-12-16 16:51:26 +0000

    [PAPI-303] Token controller route examples

---
+++ src/Opdex.Platform.WebApi/OpenApi/Tokens/DistributeOperationProcessor.cs
@@ -0,0 +1,18 @@
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+using System.Linq;
+
+namespace Opdex.Platform.WebApi.OpenApi.Tokens;
+
+public class DistributeOperationProcessor : IOperationProcessor
+{
+    public bool Process(OperationProcessorContext context)
+    {
+        var tokenAddressParameter = context.OperationDescription.Operation.Parameters.First(parameter => parameter.Name == "address");
+        tokenAddressParameter.Schema.Example = "tF83sdXXt2nTkL7UyEYDVFMK4jTuYMbmR3";
+        tokenAddressParameter.Schema.MinLength = 30;
+        tokenAddressParameter.Schema.MaxLength = 42;
+
+        return true;
+    }
+}
