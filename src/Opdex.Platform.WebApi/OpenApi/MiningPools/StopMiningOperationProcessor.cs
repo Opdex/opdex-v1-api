25e5d625162ba7bf33ce85aa3b80bcdffd64892d
drmathias <adam@opdex.com>
2021-12-20 20:31:01 +0000

    [PAPI-308] Mining pool route parameters

---
+++ src/Opdex.Platform.WebApi/OpenApi/MiningPools/StopMiningOperationProcessor.cs
@@ -0,0 +1,17 @@
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+using Opdex.Platform.Common.Models;
+using System.Linq;
+
+namespace Opdex.Platform.WebApi.OpenApi.MiningPools;
+
+public class StopMiningOperationProcessor : IOperationProcessor
+{
+    public bool Process(OperationProcessorContext context)
+    {
+        var addressParameter = context.OperationDescription.Operation.Parameters.First(parameter => parameter.Name == "address");
+        addressParameter.DefineAsNetworkAddress(new Address("tGeTZz3hnANsUre4GHB1Dy7c1EVxFjUHua"));
+
+        return true;
+    }
+}
--- src/Opdex.Platform.WebApi/OpenApi/VaultsGovernances/GetVoteOperationProcessor.cs
+++ src/Opdex.Platform.WebApi/OpenApi/VaultsGovernances/GetVoteOperationProcessor.cs
@@ -1,5 +1,6 @@
 using NSwag.Generation.Processors;
 using NSwag.Generation.Processors.Contexts;
+using Opdex.Platform.Common.Models;
 using System.Linq;
 
 namespace Opdex.Platform.WebApi.OpenApi.VaultsGovernances;
@@ -9,18 +10,14 @@ public class GetVoteOperationProcessor : IOperationProcessor
     public bool Process(OperationProcessorContext context)
     {
         var vaultAddressParameter = context.OperationDescription.Operation.Parameters.First(parameter => parameter.Name == "address");
-        vaultAddressParameter.Schema.Example = "tS1PEGC4VsovkDgib1MD3eYNv5BL2FAC3i";
-        vaultAddressParameter.Schema.MinLength = 30;
-        vaultAddressParameter.Schema.MaxLength = 42;
+        vaultAddressParameter.DefineAsNetworkAddress(new Address("tS1PEGC4VsovkDgib1MD3eYNv5BL2FAC3i"));
 
         var proposalIdParameter = context.OperationDescription.Operation.Parameters.First(parameter => parameter.Name == "proposalId");
         proposalIdParameter.Schema.Minimum = 1;
         proposalIdParameter.Schema.Example = 5;
 
         var voterParameter = context.OperationDescription.Operation.Parameters.First(parameter => parameter.Name == "voter");
-        voterParameter.Schema.Example = "tQ9RukZsB6bBsenHnGSo1q69CJzWGnxohm";
-        voterParameter.Schema.MinLength = 30;
-        voterParameter.Schema.MaxLength = 42;
+        voterParameter.DefineAsNetworkAddress(new Address("tQ9RukZsB6bBsenHnGSo1q69CJzWGnxohm"));
         return true;
     }
 }
