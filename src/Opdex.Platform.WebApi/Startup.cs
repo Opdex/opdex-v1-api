6e2df4fa19b762a69be715435a337ba80e8bfa7a
drmathias <adam@opdex.com>
2022-02-02 22:03:13 +0000

    [PAPI-360] Vary response cache by query

--- src/Opdex.Platform.WebApi/Startup.cs
+++ src/Opdex.Platform.WebApi/Startup.cs
@@ -43,6 +43,7 @@ using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Opdex.Platform.WebApi.Exceptions;
 using Opdex.Platform.Common.Encryption;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCaching;
 using Opdex.Platform.WebApi.Conventions;
 using Microsoft.Extensions.FileProviders;
 using System.Reflection;
@@ -270,6 +271,7 @@ public class Startup
         app.UseCors(options => options
                         .SetIsOriginAllowed(host => true)
                         .AllowAnyHeader()
+                        .WithExposedHeaders("Cache-Control")
                         .AllowAnyMethod()
                         .AllowCredentials());
         app.UseRouting();
@@ -277,6 +279,14 @@ public class Startup
         app.UseAuthentication();
         app.UseAuthorization();
         app.UseResponseCaching();
+        app.Use((context, next) =>
+        {
+            var responseCachingFeature = context.Features.Get<IResponseCachingFeature>();
+            // prevent cached responses when query params change
+            if (responseCachingFeature is not null) responseCachingFeature.VaryByQueryKeys = new []{ "*" };
+            return next();
+        });
+
 
         // yaml mapping not supported by default, must explicitly map
         var fileExtensionContentTypeProvider = new FileExtensionContentTypeProvider();
