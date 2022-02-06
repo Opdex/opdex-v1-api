16f2ef61784296f862f89fbc0d5de4c58334d6f1
drmathias <adam@opdex.com>
2022-02-06 15:55:55 +0000

    [PAPI-338] Suppress manual rewind tx notifications

--- src/Opdex.Platform.Application.Abstractions/EntryCommands/Transactions/CreateTransactionCommand.cs
+++ src/Opdex.Platform.Application.Abstractions/EntryCommands/Transactions/CreateTransactionCommand.cs
@@ -5,10 +5,12 @@ namespace Opdex.Platform.Application.Abstractions.EntryCommands.Transactions;
 
 public class CreateTransactionCommand : IRequest<bool>
 {
-    public CreateTransactionCommand(Sha256 txHash)
+    public CreateTransactionCommand(Sha256 txHash, bool notify)
     {
         TxHash = txHash;
+        Notify = notify;
     }
 
     public Sha256 TxHash { get; }
+    public bool Notify { get; }
 }
