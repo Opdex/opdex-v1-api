983645714e2029b4a08f1885e9ba9d6eb44866fe
Tyler Pena <tylerpena@me.com>
2022-02-20 12:17:59 -0800

    Redeem vault certificate event dto amount from Address to FixedDecimal bugfix

--- src/Opdex.Platform.Application.Abstractions/Models/TransactionEvents/Vault/RedeemVaultCertificateEventDto.cs
+++ src/Opdex.Platform.Application.Abstractions/Models/TransactionEvents/Vault/RedeemVaultCertificateEventDto.cs
@@ -6,7 +6,7 @@ namespace Opdex.Platform.Application.Abstractions.Models.TransactionEvents.Vault
 public class RedeemVaultCertificateEventDto : TransactionEventDto
 {
     public Address Holder { get; set; }
-    public Address Amount { get; set; }
+    public FixedDecimal Amount { get; set; }
     public ulong VestedBlock { get; set; }
     public override TransactionEventType EventType => TransactionEventType.RedeemVaultCertificateEvent;
 }
