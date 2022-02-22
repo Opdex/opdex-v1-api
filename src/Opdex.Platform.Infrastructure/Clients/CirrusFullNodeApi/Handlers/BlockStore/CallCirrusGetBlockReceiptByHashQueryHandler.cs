51dbe5de4f9461c56f129f805438ef534fe189be
drmathias <adam@opdex.com>
2022-02-22 16:51:38 +0000

    [PAPI-366] Ensure block receipt only includes smart contract txs

--- src/Opdex.Platform.Infrastructure/Clients/CirrusFullNodeApi/Handlers/BlockStore/CallCirrusGetBlockReceiptByHashQueryHandler.cs
+++ src/Opdex.Platform.Infrastructure/Clients/CirrusFullNodeApi/Handlers/BlockStore/CallCirrusGetBlockReceiptByHashQueryHandler.cs
@@ -34,7 +34,7 @@ public class CallCirrusGetBlockReceiptByHashQueryHandler : IRequestHandler<CallC
             if (block != null)
             {
                 return new BlockReceipt(block.Hash, block.Height, block.Time.FromUnixTimeSeconds(), block.MedianTime.FromUnixTimeSeconds(),
-                                        block.PreviousBlockHash, block.NextBlockHash, block.MerkleRoot, block.Tx);
+                                        block.PreviousBlockHash, block.NextBlockHash, block.MerkleRoot, block.SmartContractTxs);
             }
 
             if (!request.FindOrThrow) return null;
