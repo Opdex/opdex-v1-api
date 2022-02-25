5a6a1a9ff884d7bb11394005f2e0589a3c8dc8e6
drmathias <adam@opdex.com>
2022-02-25 12:31:40 +0000

    [PAPI-383] Log when transaction not retrieved from Cirrus

--- src/Opdex.Platform.Application/Handlers/Transactions/RetrieveCirrusTransactionByHashQueryHandler.cs
+++ src/Opdex.Platform.Application/Handlers/Transactions/RetrieveCirrusTransactionByHashQueryHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using Opdex.Platform.Infrastructure.Abstractions.Clients.CirrusFullNodeApi.Queries.SmartContracts;
 using Opdex.Platform.Application.Abstractions.Queries.Transactions;
 using Opdex.Platform.Domain.Models.Transactions;
@@ -14,21 +15,27 @@ namespace Opdex.Platform.Application.Handlers.Transactions;
 public class RetrieveCirrusTransactionByHashQueryHandler : IRequestHandler<RetrieveCirrusTransactionByHashQuery, Transaction>
 {
     private readonly IMediator _mediator;
+    private readonly ILogger<RetrieveCirrusTransactionByHashQueryHandler> _logger;
 
     private readonly AsyncRetryPolicy _retryPolicy = Policy
         .Handle<HttpRequestException>()
         .Or<TaskCanceledException>()
         .WaitAndRetryAsync(3, x => TimeSpan.FromSeconds(x));
 
-    public RetrieveCirrusTransactionByHashQueryHandler(IMediator mediator)
+    public RetrieveCirrusTransactionByHashQueryHandler(IMediator mediator, ILogger<RetrieveCirrusTransactionByHashQueryHandler> logger)
     {
         _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
     public async Task<Transaction> Handle(RetrieveCirrusTransactionByHashQuery request, CancellationToken cancellationToken)
     {
         var query = await _retryPolicy.ExecuteAndCaptureAsync(
             async () => await _mediator.Send(new CallCirrusGetTransactionByHashQuery(request.TxHash), cancellationToken));
-        return query.Result;
+
+        if (query.Outcome == OutcomeType.Successful) return query.Result;
+
+        _logger.LogError("Transaction could not be retrieved from Cirrus");
+        return null;
     }
 }
