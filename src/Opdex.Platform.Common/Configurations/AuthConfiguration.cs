d2d60e4847bc960a933863ad661ed4a1cb702d4b
drmathias <adam@opdex.com>
2022-04-14 15:28:30 +0100

    Remove unused JWT signing key

--- src/Opdex.Platform.Common/Configurations/AuthConfiguration.cs
+++ src/Opdex.Platform.Common/Configurations/AuthConfiguration.cs
@@ -4,18 +4,10 @@ namespace Opdex.Platform.Common.Configurations;
 
 public class AuthConfiguration : IValidatable
 {
-    public AuthProvider Opdex { get; set; }
-
     public string AdminKey { get; set; }
 
     public void Validate()
     {
-        if (string.IsNullOrEmpty(Opdex?.SigningKey)) throw new Exception("Signing key must be configured");
         if (string.IsNullOrEmpty(AdminKey)) throw new Exception("Admin key must be configured");
     }
-}
-
-public class AuthProvider
-{
-    public string SigningKey { get; set; }
 }
